667d516b1b6922b8c1e3687ea735adea09028022
SunkissedQueen <baxtercm@outlook.com>
2023-09-18 10:25:39 -0700

    3X3 board game with ?

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,25 @@
 import React from "react"
 
-const Square = () => {
+const Square = ( {board} ) => {
+  // Two options to reference props
+  // console.log("props", props.board) // when using props as the parameter, it requires dot notation to obtain a specific key
+
+  // destructure a specific property
+  // {board} - referencing the name assigned to the prop passed from App.js
+  console.log("board", board)
+  // output: ['?', '?', '?', '?', '?', '?', '?', '?', '?']
+
   return (
     <>
-      <div className="square"></div>
+      <div className="grid">
+        {board.map((square, index) => {
+          return(
+            <div className="square" key={index}>
+              {square}
+            </div>
+          )
+        })}
+      </div>
     </>
   )
 }
